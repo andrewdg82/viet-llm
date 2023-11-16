ec8a3d26345e15192fafca76e892ccdd1a934b4e
andrew <ndrew@techainer.com>
2023-11-16 04:11:54 +0000

    update create datasets

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -2,8 +2,10 @@ from datasets import load_dataset, Dataset, concatenate_datasets
 from tqdm import tqdm
 import random
 import numpy as np
+import logging
 
 def load_spoken_norm_data():
+    print(f"Loading spoken_norm_assignment dataset")
     data = []
     ds = load_dataset("VietAI/spoken_norm_assignment")
 
@@ -36,25 +38,107 @@ def load_spoken_norm_data():
                 }
                 data.append(d)
                 data.append(d2)
-
+    print(data[0])
     return Dataset.from_list(data)
 
 def load_xp3_dataset():
+    print(f"Loading bigscience/xP3 dataset")
     ds = load_dataset("bigscience/xP3", "vi")
-    ds = ds.rename_column("inputs", "input")
+    ds = ds["train"].rename_column("inputs", "input")
     ds = ds.rename_column("targets", "target")
+    print(ds[0])
     return ds
 
 def load_mfag_vi_dataset():
+    print(f"Loading nlplabtdtu/mfag_vi dataset")
     ds = load_dataset("nlplabtdtu/mfag_vi")
+    ds_train = ds["train"].rename_column("question", "input")
+    ds_train = ds_train.rename_column("answer", "target")
+
+    ds_valid = ds["validation"].rename_column("question", "input")
+    ds_valid = ds_valid.rename_column("answer", "target")
+
+    ds = concatenate_datasets([ds_train, ds_valid])
+    print(ds[0])
+    return ds
+
+def load_news_corpus_dataset():
+    ds = load_dataset("hieunguyen1053/binhvq-news-corpus")
+    print(ds)
+    data = []
+    summary_questions = [
+        "Tóm tắt đoạn văn sau: ",
+        "Tóm tắt đoạn sau: ",
+        "Tóm tắt đoạn text: ",
+        "Đoạn văn này nói về vấn đề gì: ",
+        "Hãy tổng hợp đoạn văn sau thành vài câu ngắn gọn: ",
+        "Tóm tắt: ",
+        "Rút gọn đoạn text sau: ",
+        "Viết tiêu đề cho đoạn sau: ",
+        "Viết title cho đoạn text sau: "
+    ]
+
+    classify_question = [
+        "Câu sau nói về chủ đề gì: ",
+        "Đoạn văn sau nói về chủ đề gì: ",
+        "Chỉ ra chủ đề của đoạn văn sau: ",
+        "Gán nhãn chủ đề cho câu sau: ",
+        "Lĩnh vực được đề cập đến của câu sau là gì: ",
+        "Đoạn văn sau nói về lĩnh vực gì: "
+    ]
+
+    for item in tqdm(ds["train"]):
+        content = item["content"]
+        content = content.split("\n")[:-3]
+        item["content"] = "\n".join(content)
+        sample_write_content = {
+            "input": item["title"],
+            "target": item["content"]
+        }
+        sample_summary = {
+            "input": random.choice(summary_questions) + item["content"],
+            "target": item["title"] if np.random.rand() > 0.5 else item["summary"]
+        }
+        sample_classify = {
+            "input": random.choice(classify_question) + item["content"] if np.random.rand() > 0.5 else item["summary"],
+            "target": item["category"]
+        }
+        sample_classify2 = {
+            "input": random.choice(classify_question) + item["title"] if np.random.rand() > 0.5 else item["summary"],
+            "target": item["category"]
+        }
+        data += [sample_write_content, sample_summary, sample_classify, sample_classify2]
+
+    ds = Dataset.from_list(data)
+    return ds
 
 def load_json(filepath: str):
+    print(f"Loading dataset from json file: {filepath}")
     ds = load_dataset("json", data_files=filepath)
+    ds = ds["train"].rename_column("question", "input")
+    ds = ds.rename_column("answer", "target")
+    ds = ds.filter(lambda item: len(item["input"].split()) > 3 and len(item["target"].split()) > 2)
+    return ds
+
+def clean_text(text: str) -> str:
+    return text
 
 def create_dataset(save_filepath: str):
-    # ds = load_spoken_norm_data()
-    # ds = load_xp3_dataset()
-    ds = load_mfag_vi_dataset()
+    list_dataset = [
+        load_spoken_norm_data(),
+        load_xp3_dataset(),
+        load_mfag_vi_dataset(),
+        load_json("data/data_v1_426k.json"),
+        load_news_corpus_dataset()
+    ]
+    print(list_dataset)
+    concat_ds = concatenate_datasets(list_dataset)
+    print(concat_ds)
+    concat_ds = concat_ds.filter(lambda item: len(item["input"].split()) > 3 and len(item["target"].split()) > 2)
+    print(concat_ds)
+
+    concat_ds.to_csv(save_filepath)
+
 
 if __name__=="__main__":
-    create_dataset("data/data.json")
+    create_dataset("data/data.csv")
